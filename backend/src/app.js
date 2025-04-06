430676c911fb329c92da61959b636fe3e0e24611
Payel647 <mallickkoyel098@gmail.com>
2025-04-06 19:15:52 +0530

    configure setting  commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { createServer } from "node:http";
 import mongoose from "mongoose";
-import { connectTosocket } from "./controller/soketmaneger.js";
+import { connectTosocket } from "./controllers/soketmaneger.js";
 
 import cors from "cors";
-
+import userRoutes from "./routes/users.routes.js";
 const app = express();
 const server = createServer(app);
 const io = connectTosocket(server);
 app.set("port",(process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({limit: "40kb",extended: true}));
+app.use("/api/v1/users",userRoutes);
 
 app.get("/home", (req, res) => {
  return res.json({"hello": "world"})
